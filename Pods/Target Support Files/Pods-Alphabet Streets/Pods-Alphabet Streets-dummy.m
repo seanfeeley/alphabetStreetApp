7c89b524be6146ec3cb016d29802f2ec15163ac4
seanfeeley <feeley19@gmail.com>
2016-11-01 22:38:47 +0000

    got running on ipad, lost alot of work during the conversion

---
+++ Pods/Target Support Files/Pods-Alphabet Streets/Pods-Alphabet Streets-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Alphabet_Streets : NSObject
+@end
+@implementation PodsDummy_Pods_Alphabet_Streets
+@end
---
+++ Pods/Target Support Files/Pods-Alphabet Streets/Pods-Alphabet Streets-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Alphabet_StreetsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Alphabet_StreetsVersionString[];
+
